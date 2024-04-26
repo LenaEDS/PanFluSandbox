a6d5213c3a9d01984c0bceea5291ce91a42d4139
gladyschenyx <98440939+gladyschenyx@users.noreply.github.com>
2024-04-26 15:19:18 -0500

    Configured layout of components

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Simulator from './Simulator';
 import TexasMap from './components/TexasMap';
 import Parameters from './components/Parameters';
 import IncrementDecrement from './components/IncrementDecrement';
+import Panel from './components/Panel';
 
 function App() {
   return (
     <div className="App">
-
+    <header>
+      <h1>Flu Simulator</h1>
+    </header>
+    <div className="content">
       <TexasMap />
-        <div>
-          <Parameters/>   
-        </div>
-
-      <h1>Increment</h1>
-      <IncrementDecrement />
+      <div className="parametersContainer">
+      <Panel>
+          <Parameters />
+      </Panel>
+      </div>
     </div>
+  </div>
   );
 }
 
