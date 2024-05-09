d7494aed069872b859cfeb5db27e7f732b6f214c
gladyschenyx <98440939+gladyschenyx@users.noreply.github.com>
2024-05-09 13:46:10 -0500

    Added scrolling feature for infected counts across counties

--- src/components/CountyInfectedTable.js
+++ src/components/CountyInfectedTable.js
@@ -5,27 +5,29 @@ import countyInfectedData from './countyInfectedData';
 import '../App.css'; // Import the CSS file
 
 function CountyInfectedTable() {
-  return (
-    <div className="table-container">
-      <h3>Infected Count </h3>
-      <table>
-        <thead>
-          <tr>
-            <th>County</th>
-            <th>Infected</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countyInfectedData.map((county, index) => (
-            <tr key={index}>
-              <td>{county.county}</td>
-              <td>{county.infected}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-export default CountyInfectedTable;
+    return (
+      <div className="table-container">
+        <h2>Infected Count</h2>
+        <div className="table-scroll">
+          <table>
+            <thead>
+              <tr>
+                <th>County</th>
+                <th>Infected</th>
+              </tr>
+            </thead>
+            <tbody>
+              {countyInfectedData.map((county, index) => (
+                <tr key={index}>
+                  <td>{county.county}</td>
+                  <td>{county.infected}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+  
+  export default CountyInfectedTable;
