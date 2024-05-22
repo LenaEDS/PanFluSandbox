18056c834a2a9515048298348b3a89b880cc90f6
Lena Duplechin Seymour <68430483+LenaEDS@users.noreply.github.com>
2024-05-22 11:36:11 -0500

    Initial binomial travel model

--- simulator/src/models/disease/SEATIRD.py
+++ simulator/src/models/disease/SEATIRD.py
@@ -1,17 +1,41 @@
 import numpy as np
 
-#Local aliases to get around compiler issues.
+from enum import Enum
 
+
+class Compartments(Enum):
+    SUSCEPTIBLE = 1
+    EXPOSED = 2
+    ASYMPTOMATIC = 3
+    TREATABLE = 4
+    INFECTIOUS = 5
+    RECOVERED = 6
+    DECEASED = 7 
+
+
+Compartments.SUSCEPTIBLE...
+
+if(thing_to_test == Compartments.SUSCEPTIBLE):
+    do the thing
+
+
+#Local aliases to get around compiler issues.
+##write out full names for clarity for future
 S = COMPARTMENTS_SUSCEPTIBLE
 E = COMPARTMENTS_EXPOSED
-A = COMPARTMENTS_ASYMTOMATIC
+A = COMPARTMENTS_ASYMPTOMATIC
 T = COMPARTMENTS_TREATABLE
 I = COMPARTMENTS_INFECTIOUS
 R = COMPARTMENTS_RECOVERED
-D = COMPARTMENTS_DECASED
+D = COMPARTMENTS_DECEASED
 
 #Constructor.
 
+class SEATIRD:
+    def __init__ (self):
+        return
+
+##2nd (copy) constructor might not be needed?
 
 #Destructor.
 
@@ -26,5 +50,3 @@ D = COMPARTMENTS_DECASED
 # 3 parameters, node, g, and number
 # node is a pointer to object type 'Node'
 #g is an object of type 'GROUP'
-
-#test
