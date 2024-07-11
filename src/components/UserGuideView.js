2e25ccf345e2dcf210ae26df3bbcc61c021d1ef9
gladyschenyx <98440939+gladyschenyx@users.noreply.github.com>
2024-07-11 17:21:55 -0500

    Animation of infected map with red color scheme

--- src/components/UserGuideView.js
+++ src/components/UserGuideView.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import InitialMap from './InitialMap';
-import InitialMapTwo from './InitialMapTwo';
-import InitialMapThree from './InitialMapThree';
-import InitialMapFour from './InitialMapFour';
+import React, { useState } from 'react';
 import InitialMapFive from './InitialMapFive';
+import InitialMap from './InitialMap';
 import OUTPUT_1 from './OUTPUT_1.json';
 import OUTPUT_2 from './OUTPUT_2.json';
 import OUTPUT_3 from './OUTPUT_3.json';
@@ -13,35 +10,49 @@ import OUTPUT_6 from './OUTPUT_6.json';
 import OUTPUT_7 from './OUTPUT_7.json';
 import OUTPUT_8 from './OUTPUT_8.json';
 import OUTPUT_9 from './OUTPUT_9.json';
+import OUTPUT_10 from './OUTPUT_10.json';
+import OUTPUT_11 from './OUTPUT_11.json';
+import OUTPUT_12 from './OUTPUT_12.json';
+import OUTPUT_13 from './OUTPUT_13.json';
+import OUTPUT_14 from './OUTPUT_14.json';
+import OUTPUT_15 from './OUTPUT_15.json';
+import OUTPUT_16 from './OUTPUT_16.json';
+import OUTPUT_17 from './OUTPUT_17.json';
+import OUTPUT_18 from './OUTPUT_18.json';
+import OUTPUT_19 from './OUTPUT_19.json';
+import OUTPUT_20 from './OUTPUT_20.json';
+import OUTPUT_21 from './OUTPUT_21.json';
+import OUTPUT_22 from './OUTPUT_22.json';
+import OUTPUT_23 from './OUTPUT_23.json';
+import OUTPUT_24 from './OUTPUT_24.json';
+import OUTPUT_25 from './OUTPUT_25.json';
+import OUTPUT_26 from './OUTPUT_26.json';
+import OUTPUT_27 from './OUTPUT_27.json';
+import OUTPUT_28 from './OUTPUT_28.json';
 
 const UserGuideView = () => {
+  const [currentIndex, setCurrentIndex] = useState(1); // Initialize currentIndex state
+  const maxIndex = 30; // Maximum index of outputData
+
+  const handlePlay = () => {
+    // Function to play animation
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex % maxIndex) + 1); // Cycle through indices
+    }, 1000); // Interval duration in milliseconds
+
+    // Stop animation after all indices are shown
+    setTimeout(() => {
+      clearInterval(interval);
+    }, maxIndex * 1000); // Stop after maxIndex seconds
+  };
+
   return (
     <div>
-      <h2>User Guide View</h2>
-      <InitialMapFive outputData={OUTPUT_9} />
-      <InitialMapFour outputData={OUTPUT_9} />
-      <InitialMapThree outputData={OUTPUT_9} />
-      <InitialMapTwo outputData={OUTPUT_9} />
-      <InitialMap outputData={OUTPUT_9} /> 
-      <InitialMap outputData={OUTPUT_1} />
-      <InitialMap outputData={OUTPUT_2} />
-      <InitialMap outputData={OUTPUT_3} />
-      <InitialMap outputData={OUTPUT_4} />
-      <InitialMap outputData={OUTPUT_5} />
-      <InitialMap outputData={OUTPUT_6} />
-      <InitialMap outputData={OUTPUT_7} />
-      <InitialMap outputData={OUTPUT_8} />
-      <InitialMap outputData={OUTPUT_9} />
-      <InitialMapFive outputData={OUTPUT_1} />
-      <InitialMapFive outputData={OUTPUT_2} />
-      <InitialMapFive outputData={OUTPUT_3} />
-      <InitialMapFive outputData={OUTPUT_4} />
-      <InitialMapFive outputData={OUTPUT_5} />
-      <InitialMapFive outputData={OUTPUT_6} />
-      <InitialMapFive outputData={OUTPUT_7} />
-      <InitialMapFive outputData={OUTPUT_8} />
-      <InitialMapFive outputData={OUTPUT_9} />
+      {/* Play button */}
+      <button onClick={handlePlay}>Play</button>
 
+      {/* Render InitialMapFive component with dynamic outputData */}
+      <InitialMap outputData={require(`./OUTPUT_${currentIndex}.json`)} />
     </div>
   );
 };
