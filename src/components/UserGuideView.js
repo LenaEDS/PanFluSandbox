5c2cfca016c8fc41025482a7445de186da08a248
gladyschenyx <98440939+gladyschenyx@users.noreply.github.com>
2024-07-17 15:09:50 -0500

    Setting up testing orientation for infected table, map and slider

--- src/components/UserGuideView.js
+++ src/components/UserGuideView.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect, useRef } from 'react';
 import InitialMap from './InitialMap';
 import InitialMapFive from './InitialMapFive';
 import TimelineSlider from './TimelineSlider';
+import CountyInfectedTable from './CountyInfectedTable';
+import './UserGuideView.css';
+import OUTPUT_1 from './OUTPUT_1.json'
+import OUTPUT_2 from './OUTPUT_2.json'
+import OUTPUT_3 from './OUTPUT_3.json'
+import OUTPUT_4 from './OUTPUT_4.json'
+import OUTPUT_5 from './OUTPUT_5.json'
+import OUTPUT_6 from './OUTPUT_6.json'
+import OUTPUT_7 from './OUTPUT_7.json'
+import OUTPUT_8 from './OUTPUT_8.json'
+import OUTPUT_9 from './OUTPUT_9.json'
+import OUTPUT_10 from './OUTPUT_10.json'
+import OUTPUT_11 from './OUTPUT_11.json'
+import OUTPUT_12 from './OUTPUT_12.json'
+import OUTPUT_13 from './OUTPUT_13.json'
+import OUTPUT_14 from './OUTPUT_14.json'
+import OUTPUT_15 from './OUTPUT_15.json'
 
-const importAll = async (r) => {
+async function importAll(r) {
   const files = r.keys();
   const modules = await Promise.all(files.map(r));
   return modules;
-};
+}
 
 const UserGuideView = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,7 +35,7 @@ const UserGuideView = () => {
   useEffect(() => {
     const loadFiles = async () => {
       try {
-        const files = await importAll(require.context('./', false, /\.json$/));
+        const files = await importAll(require.context('../output', false, /\.json$/));
         setOutputFiles(files);
         console.log('Files loaded:', files); // Print all loaded files
         setLoading(false);
@@ -72,16 +89,23 @@ const UserGuideView = () => {
   }
 
   return (
-    <div>
-      <InitialMap outputData={outputFiles[currentIndex]} />
-      <InitialMapFive outputData={outputFiles[currentIndex]} />
-      <TimelineSlider
-        totalDays={outputFiles.length}
-        selectedDay={currentIndex}
-        onDayChange={handleDayChange}
-        onScenarioRun={handleRunScenario}
-        onScenarioPause={handlePauseScenario}
-      />
+    <div className="user-guide-view">
+      <div className="left-panel">
+        <CountyInfectedTable className="infected-table" outputData={outputFiles[currentIndex]} />
+      </div>
+      <div className="right-panel">
+        <InitialMap outputData={outputFiles[currentIndex]} />
+        <InitialMapFive outputData={outputFiles[currentIndex]} />
+      </div>
+      <div className="bottom-panel">
+        <TimelineSlider
+          totalDays={outputFiles.length}
+          selectedDay={currentIndex}
+          onDayChange={handleDayChange}
+          onScenarioRun={handleRunScenario}
+          onScenarioPause={handlePauseScenario}
+        />
+      </div>
     </div>
   );
 };
